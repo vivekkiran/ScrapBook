8888c851febb74dcdc4580a574aadcc25f1c31ea
Vivek Kiran <vivek.kiran@outlook.com>
2015-04-19 22:55:58 +0530

    Code Optimization and Perfomance Improvements

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,2 +1,3 @@
-APP_ABI := armeabi armeabi-v7a mips
+APP_ABI := all
 APP_PLATFORM := android-8
+#This will trigger Android-NDK's build script to compile the source code for all the chipsets dynamically.
